f31ac418674839b5c6820ba249036b69a952a8a2
jblancor12 <frikimcpelon@gmail.com>
2020-09-09 19:16:55 -0600

    Euler.py

--- euler.py
+++ euler.py
@@ -1,8 +1,25 @@
+import math 
 def e_cuadratica(n):
-    # Implemente esta función
-    return 0
+    
+    a=0
+    for i in range(n):
+    	a=a+1/math.factorial(i)
 
+
+
+    return a
 
 def e_lineal(n):
-    # Implemente esta función
-    return 0
+  
+    fact=[1]
+    index=1
+    while index<n+1:
+    	fact.append(fact[index-1]*index)
+    	index=index+1
+
+    res=0
+    for i in range (len(fact)-1):
+    	res=res+1/fact[i]
+
+
+    return res
